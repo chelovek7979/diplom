5a640a204b1255ac9914485f2ef8a697ce332fe5
chelovek7979 <Dyger3579@gmail.com>
2026-05-13 05:32:49 +0300

    deploy

--- Server/db.js
+++ Server/db.js
@@ -1,4 +1,4 @@
-import mysql from "mysql2";
+/* import mysql from "mysql2";
 
 export const db = mysql.createConnection({
   host: "localhost",
@@ -16,3 +16,25 @@ db.connect(err => {
   }
 });
 
+ */
+
+// db.js
+import mysql from "mysql2";
+
+// Подключение к базе через переменные окружения
+export const db = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "root",
+  database: process.env.DB_NAME || "Diplom_bd",
+  port: Number(process.env.DB_PORT) || 3307
+});
+
+// Проверка подключения
+db.connect(err => {
+  if (err) {
+    console.error("Ошибка подключения к базе:", err);
+  } else {
+    console.log("База данных подключена ");
+  }
+});
