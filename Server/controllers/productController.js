4f375c5f7e13a479ac5c809f93bbeb6ad5e2597b
chelovek7979 <Dyger3579@gmail.com>
2026-05-16 02:08:59 +0300

    kek

--- Server/controllers/productController.js
+++ Server/controllers/productController.js
@@ -3,7 +3,6 @@ import { db } from "../db.js";
 // 📦 Получить все товары
 export const getProducts = (req, res) => {
   const query = "SELECT * FROM products";
-
   db.query(query, (err, data) => {
     if (err) return res.status(500).json(err);
     return res.json(data);
@@ -13,69 +12,67 @@ export const getProducts = (req, res) => {
 // 📦 Получить один товар
 export const getProductById = (req, res) => {
   const query = "SELECT * FROM products WHERE idProduct = ?";
-
   db.query(query, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
     return res.json(data[0]);
   });
 };
 
-// ➕ Добавить товар
-
-  export const createProductWithImage = async (req, res) => {
+// ➕ Добавить товар с файлом
+export const createProductWithImage = async (req, res) => {
   try {
     console.log("req.file:", req.file);
     console.log("req.body:", req.body);
-    res.json({ message: "Файл дошёл до сервера" });
+
+    if (!req.file) {
+      return res.status(400).json({ message: "Файл обязателен" });
+    }
+
+    // Ссылка на файл на сервере
+    const Product_image_url = `/uploads/${req.body.Product_category}/${req.file.filename}`;
+
+    const query = `
+      INSERT INTO products 
+        (Product_title, Product_description, Product_price, Product_image_url, Product_category, product_count)
+      VALUES (?, ?, ?, ?, ?, ?)
+    `;
+
+    db.query(
+      query,
+      [
+        req.body.Product_title,
+        req.body.Product_description,
+        req.body.Product_price,
+        Product_image_url,
+        req.body.Product_category,
+        req.body.product_count,
+      ],
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ message: "Ошибка базы данных" });
+        }
+        res.json({
+          message: "Товар добавлен",
+          id: result.insertId,
+          image: Product_image_url
+        });
+      }
+    );
   } catch (err) {
     console.error("Ошибка контроллера:", err);
     res.status(500).json({ message: "Ошибка сервера", error: err.message });
   }
 };
 
-  if (!req.file) return res.status(400).json({ message: "Файл обязателен" });
-
-  // Ссылка на файл в Cloudinary
-  const Product_image_url = req.file.path;
-
-  const query = `
-    INSERT INTO products 
-      (Product_title, Product_description, Product_price, Product_image_url, Product_category, product_count)
-    VALUES (?, ?, ?, ?, ?, ?)
-  `;
-
-  db.query(
-    query,
-    [
-      req.body.Product_title,
-      req.body.Product_description,
-      req.body.Product_price,
-      Product_image_url,           // сохраняем ссылку на Cloudinary
-      req.body.Product_category,
-      req.body.product_count,
-    ],
-    (err, result) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ message: "Ошибка базы данных" });
-      }
-      res.json({ 
-        message: "Товар добавлен", 
-        id: result.insertId, 
-        image: Product_image_url  // возвращаем URL для фронта
-      });
-    }
-  );
-
-
 // ✏️ Обновить товар
 export const updateProduct = (req, res) => {
   const { Product_title, Product_description, Product_price, Product_image_url, Product_category, product_count } = req.body;
 
   const query =
-    "UPDATE products SET Product_title = ?, Product_description = ?, Product_price = ?,Product_image_url = ?, Product_category = ?, product_count = ? WHERE  idProduct = ?";
+    "UPDATE products SET Product_title = ?, Product_description = ?, Product_price = ?, Product_image_url = ?, Product_category = ?, product_count = ? WHERE idProduct = ?";
 
-  db.query(query, [Product_title, Product_description,Product_price,Product_image_url,Product_category,product_count,  req.params.id], (err) => {
+  db.query(query, [Product_title, Product_description, Product_price, Product_image_url, Product_category, product_count, req.params.id], (err) => {
     if (err) return res.status(500).json(err);
     return res.json({ message: "Товар обновлён" });
   });
@@ -84,7 +81,6 @@ export const updateProduct = (req, res) => {
 // ❌ Удалить товар
 export const deleteProduct = (req, res) => {
   const query = "DELETE FROM products WHERE idProduct = ?";
-
   db.query(query, [req.params.id], (err) => {
     if (err) return res.status(500).json(err);
     return res.json({ message: "Товар удалён" });
